29f3a292e733ee70ef8838fa28230de458c232c8
XCodeBlocks <xcodeblocks@gmail.com>
2017-05-29 10:35:04 +0900

    Lotto 클래스 - (랜덤 생성 -- 숫자 1개) (getRandom 메소드)

--- app/src/main/java/xcodeblocks/lotto/Lotto.java
+++ app/src/main/java/xcodeblocks/lotto/Lotto.java
@@ -4,6 +4,10 @@ package xcodeblocks.lotto;
 
 /**
  * Created by XCodeBlocks on 2017-05-29.
+ *
+ * --(reference):
+ * -(숫자 랜덤 생성): http://www.java67.com/2015/01/how-to-get-random-number-between-0-and-1-java.html
+ *
  */
 
 public class Lotto {
@@ -14,11 +18,17 @@ public class Lotto {
 
 //[생성자(constructor)]
     public Lotto() {
+        //[]
 
+        //[]
+
+        //[]
     }
 
-//[랜덤 생성]
-
+//[랜덤 생성 -- 숫자 1개]
+    public static int getRandom(int max) {
+        return (int) (Math.random() * max);       //(원래 double형 결과가 나오는 거라서)
+    }
 
 //[선택 정렬] - (오름차순)
     void selectionSort(int[] numbers)
