4ca3c7657cdb4ff37231f59523a0161b42b13da2
XCodeBlocks <xcodeblocks@gmail.com>
2017-06-10 11:21:59 +0900

    MainActivity.java (소소한 변경) -- (별 중요한 것은 없음)

--- app/src/main/java/xcodeblocks/lotto/MainActivity.java
+++ app/src/main/java/xcodeblocks/lotto/MainActivity.java
@@ -5,6 +5,9 @@
 
 /** [references]:
  *
+ *  //[방향]: 일단 그때그때 필요한 변수들을 그 자리에서 부르고(정말 그렇게 하면 않되는 경우에만 다른 곳에 선언) 진행하고 나서
+ *            어느정도 (코드가) 되었다 싶을때 선언들만 (메소드들) 밖으로 빼서 (클래스) 전역으로 선언. -- (처음부터 어렵다면)
+ *
  * - (logcat 사용 - 출력해서 확인하는 방법):
  * https://stackoverflow.com/questions/38289910/print-on-console-in-android-studio
  * ㄴ (실제로는 아래 코드처럼 사용)
@@ -32,17 +35,16 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'native-lib' library on application startup.
-
 /*  static {
       System.loadLibrary("native-lib");
     }
 */
-//(전역 변수 선언)
+//[전역 변수 선언]
     int NUMBER_SEL = 6;             //FIXME: (Lotto.java(클래스)에서 전역으로 선언한 변수가 인식이 않됨!!) -- (-> 여기서 지역변수로 재선언)
-    Lotto lottoTest = new Lotto();              //(객체 생성 (+숫자 생성) )
+    Lotto lottoTest = new Lotto();                          //(객체 생성 (+숫자 생성) )
     //[숫자들 출력하는 TextView 선언+초기화]
     TextView[] numbers_TextView = new TextView[NUMBER_SEL];               //TODO: [왜 전역변수인 NUMBER_SEL 인식이 않되는가 ?!]
-    int[] numberArray = lottoTest.getNumbers();     //(getNumbers 메소드에 [] 잇는 대신에 저장할 별도의 숫자 배열 -- 시각상)
+    int[] numberArray = lottoTest.getNumbers();     //(getNumbers 메소드에 [] 잇는 대신에 저장할 별도의 숫자 배열 -- 시각상(지저분한 것 같아서))
 
 
     @Override
@@ -51,18 +53,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        setTitle("로또 번호 생성 앱");             //(제목(맨 윗줄) 내용 변경)
 
-//[각각의 TextView 객체에 할당]     -- (선언은 위에서 미리)
+//[각각의 TextView 객체 할당]     -- (선언은 위에서 미리...)
         numbers_TextView[0] = (TextView) findViewById(R.id.numbers0);
         numbers_TextView[1] = (TextView) findViewById(R.id.numbers1);
         numbers_TextView[2] = (TextView) findViewById(R.id.numbers2);
         numbers_TextView[3] = (TextView) findViewById(R.id.numbers3);
         numbers_TextView[4] = (TextView) findViewById(R.id.numbers4);
         numbers_TextView[5] = (TextView) findViewById(R.id.numbers5);
-/* TODO: **<긴급>: (여기서 에러 않 나게) 적당히 내용 없는 문자열 (객체)를(이라도) (each가) 가리키도록 하여 nullPointerException을 고친다!...(이어짐)
-                -- 처음에 빈칸만 출력되게 하는 부분 고칠 것!!] **
- */
-
 
 //(확장형 for문: 빈칸으로 초기화)
         for (TextView each: numbers_TextView) {     //(여기서 '위의 명령'을 이런 식으로 반복할 수 없어서 위로 뺌)
@@ -78,6 +77,8 @@ public class MainActivity extends AppCompatActivity {
             str += ( eachString + " ");         //(...로그 출력에 필요한 String 형태로 변환 + (숫자들 구별되게 띄어쓰기 추가))
         }
         Log.i("String", "디버그: " + str );                        //(logcat으로 출력)
+
+
 
 // TODO: 2017-06-04: UI: 숫자 출력할 상자에 숫자 배열 값들을 각각 연결시키기
 
